04552615b6c31e2b083b829f9561ee5a0bce8405
np595 <49132942+np595@users.noreply.github.com>
2020-02-23 18:51:06 -0800

    Create Problem1Recursive.java

---
+++ Problem1Recursive.java
@@ -0,0 +1,20 @@
+class Problem1Recursive {
+
+    class Node { 
+        int key; 
+        Node left, right; 
+  
+        public Node(int item) { 
+            key = item; 
+            left = null;
+            right = null; 
+        } 
+    } 
+    
+    Node insert(Node root, int key){
+    
+        
+    
+    }
+
+}
