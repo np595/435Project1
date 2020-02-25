745e2319244f5d35311ad36846666830e456b25d
np595 <49132942+np595@users.noreply.github.com>
2020-02-25 13:20:30 -0800

    Update Problem3a.java

--- Problem3a.java
+++ Problem3a.java
@@ -1,1 +1,22 @@
+import java.util.Random; 
 
+class Problem3a{
+
+    public static void main(String[] args){
+    
+        Random rand = new Random();
+      
+        int i;
+        int size = rand.nextInt(20); //Makes a random size
+      
+        int[] arr =  new int[size];
+      
+        for(i = 0; i < size; i++){
+        
+            arr[i] = rand.nextInt(100);
+        
+        }
+    
+    }
+
+}
