05f39a39038533750ca16d7539a0efec05063e82
np595 <49132942+np595@users.noreply.github.com>
2020-02-24 21:43:41 -0800

    Update Problem2c.java

--- Problem2c.java
+++ Problem2c.java
@@ -1,1 +1,21 @@
-class 
+class Problem2c{
+
+    
+  
+    Node sort(int arr[], int min, int max) { 
+
+        if (min > max) { //If min is greated than max, then return null since it will not sort into a correct BST
+            return null; 
+        } 
+      
+        int mid = (start + end) / 2; 
+        Node node = new Node(arr[mid]); 
+
+        node.left = sortedArrayToBST(arr, start, mid - 1); 
+
+        node.right = sortedArrayToBST(arr, mid + 1, end); 
+          
+        return node; 
+    }
+
+}
