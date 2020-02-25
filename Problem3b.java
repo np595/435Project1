ad4467858393cde40c2871f4340a892405779e4d
np595 <49132942+np595@users.noreply.github.com>
2020-02-25 13:42:37 -0800

    Update Problem3b.java

--- Problem3b.java
+++ Problem3b.java
@@ -1,10 +1,37 @@
+import java.util.Random; 
 
-//The program asks for a sorted array that is resorted to be 0 in the array equalling n or the last number of the original array and so on
+//Sorts program array from size to zero
 class Problem3b{
 
+    
+    
     public static void main(String[] args){
     
+        Random rand = new Random();
+      
+        int i, j;
+        int size = rand.nextInt(20); //Makes a random size
+      
+        int[] arr =  new int[size];
+        int[] newArr = new int[size];
+      
+        for(i = 0; i < size; i++){
         
+            arr[i] = rand.nextInt(100);
+        
+        }
+        
+        for(i = size - 1, j = 0; i > 0; i--, j++){
+        
+            newArr[j] = arr[i];
+        
+        }
+        
+        for(i = 0; i < size; i++){
+        
+            System.out.println(newArr[i]);
+        
+        }
     
     }
 
