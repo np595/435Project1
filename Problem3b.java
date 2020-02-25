8f733563d9e67b82b4082c97e6ea45df9786f05d
np595 <49132942+np595@users.noreply.github.com>
2020-02-25 13:39:38 -0800

    Update Problem3b.java

--- Problem3b.java
+++ Problem3b.java
@@ -1,7 +1,11 @@
 
-
+//The program asks for a sorted array that is resorted to be 0 in the array equalling n or the last number of the original array and so on
 class Problem3b{
 
-    public static void main(
+    public static void main(String[] args){
+    
+        
+    
+    }
 
 }
