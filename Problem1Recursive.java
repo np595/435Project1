2a0b4bacee7a2ad70f7f52a906bf35e0085acfa2
np595 <49132942+np595@users.noreply.github.com>
2020-03-02 17:53:37 -0800

    Update Problem1Recursive.java

--- Problem1Recursive.java
+++ Problem1Recursive.java
@@ -1,121 +1,176 @@
 class Problem1Recursive {
 
-    class Node { 
-        int key; 
-        Node left, right; 
-  
-        public Node(int item) { 
-            key = item; 
-            left = null;
-            right = null; 
-        } 
-    } 
-    
-    Node root;
-    
-    Problem1Recursive(){
-        
-        Node root = null;
-        
-    }
-    
-    void insert(int key){
-    
-        root = insertN(root, key);
-    
-    }
-    
-    Node insertN(Node root, int key){
-    
-        if(root == null){ //Check if tree is empty and return the number as a new node
-            
-            root = new Node(key);
-            return root;
-        
-        }
-        
-        //Else continue until left or right is found null
-        if (key < root.key) 
-            root.left = insertN(root.left, key); 
-        else if (key > root.key) 
-            root.right = insertN(root.right, key);
-        
-        return root;
-    
-    }
-    
-    void remove(int key){
-    
-        root = removeN(root, key);
-    
-    }
-    
-    Node removeN(Node root, int key){
-    
-        if(root == null)
-            return root;
-        
-        if(key < root.key)
-            root.left = removeN(root.left, key);
-        else if(key > root.key)
-            root.right = removeN(root.right, key);
-        
-        else{ //if root.key = key then that's the one we want to delete
-        
-            if (root.left == null) //Check for any leaves or children to then move up once deleted
-                return root.right; 
-            else if (root.right == null) 
-                return root.left;
-            
-            root.right = removeN(root.right, root.key); 
-        
-        }
-                
-        return root;
-    
-    }
-    
-    Node findNext(Node root){ //Proceeds down the tree for the next highest value
-    
-        
-    
-    }
-    
-    Node findPrev(Node root){ //Proceeds up the tree to the limit of the root
-    
-        
-    
-    }
-    
-    int findMin(Node root) { 
-        Node current = root; 
-        
-        while (current.left != null) { //Recursively look through the data until the absolute left three has been found which holds the min
-            
-            current = current.left; 
-            
-        } 
-        return current.key; 
-    } 
+        class Node {
+                int key;
+                Node left, right;
 
-    int findMax(Node root){
-    
-        Node current = root;
-        
-        while(current.right != null){ //Continues until the absolute right has been found for the max value
-        
-            current = current.right;
-            
+                public Node(int item) {
+                        key = item;
+                        left = null;
+                        right = null;
+                }
         }
-        
-        return current.key;
-        
-    }
-    
-    public static void main(String[] args){
-    
-        
-    
-    }
-    
+
+        Problem1Recursive(){
+
+                Node root = null;
+
+        }
+
+        static void insert(int key){
+
+                root = insertN(root, key);
+
+        }
+
+        static Node insertN(Node root, int key){
+
+                if(root == null){ //Check if tree is empty and return the number as a new node
+                        root = new Node(key);
+                        return root;
+                }
+
+                if (key < root.key)
+                        root.left = insertN(root.left, key);
+                else if (key > root.key)
+                        root.right = insertN(root.right, key);
+
+                return root;
+        }
+
+        static void remove(int key){
+                root = removeN(root, key);
+        }
+
+        static Node removeN(Node root, int key){
+                if(root == null)
+                        return null;
+
+                if(key < root.key)
+                        root.left = removeN(root.left, key);
+                else if(key > root.key)
+                        root.right = removeN(root.right, key);
+
+                else{
+                        if(root.left == null)
+                                return root.right;
+                        else if(root.right == null)
+                                return root.left;
+
+                        root.right = removeN(root.right, root.key);
+                }
+
+                return root;
+        }
+
+        static void findNext(int key){
+
+                root = findNextSuc(root, key);
+
+        }
+
+        static Node findNextSuc(Node root, int key){
+
+                if(root == null)
+                        return null;
+
+                if(root.right != null){
+                        Node temp = root.right;
+                        while(temp.left != null)
+                                temp = temp.left;
+                        return temp;
+                }
+                else{
+                        Node successor = null;
+                        Node curr = root;
+                        while(curr != root){
+                                if(root.key < curr.key){
+                                        successor = curr;
+                                        curr = curr.left;
+                                }
+                                else
+                                        curr = curr.right;
+                        }
+                        return successor;
+                }
+
+        }
+
+        static void findPrev(int key){
+
+                root = findPrevSuc(root, key);
+
+        }
+
+        static Node findPrevSuc(Node root, int key){
+
+                if(root == null)
+                        return root;
+
+                if(root.left != null){
+                        Node temp = root.left;
+                        while(temp.right != null)
+                                temp = temp.right;
+                        return temp;
+                }
+                else{
+                        Node successor = null;
+                        Node curr = root;
+                        while(curr != root){
+                                if(root.key < curr.key){
+                                        successor = curr;
+                                        curr = curr.right;
+                                }
+                                else
+                                        curr = curr.left;
+                        }
+                        return successor;
+                }
+
+        }
+
+        static int findMin(Node root){
+
+                Node current = root;
+
+                while(current.left != null){
+
+                        current = current.left;
+
+                }
+
+                return current.key;
+
+        }
+
+        static int findMax(Node root){
+
+                Node current = root;
+
+                while(current.right != null){
+                        current = current.right;
+                }
+                return current.key;
+
+        }
+
+        public static void main(String[] args){
+
+                insert(5);
+                insert(10);
+                insert(15);
+                insert(23);
+                insert(12);
+                insert(24);
+
+                remove(10);
+
+                findMin(root);
+                findMax(root);
+
+                findPrev(1);
+                findNext(1);
+
+        }
+
 }
